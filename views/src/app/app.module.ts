22e92f2eae0c90dbd6da79c8695e8e7e35e83091
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2021-01-14 22:30:23 +1100

    Create basic landing page layout
    Using Card and Tabs Material components.

--- views/src/app/app.module.ts
+++ views/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckInComponent } from './check-in/check-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
