106e158037e20350523cf6d5e824b6680a9a538a
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2021-01-21 00:15:26 +1100

    Remove TypeScript functions
    Due to lack of documentation (in finding the right types) and community support, I will switch to JavaScript to write the Functions.

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-const app = require('express')();
-
-// Boards
-const { getAllEvents } = require('./apis/events');
-app.get('/events', getAllEvents);
-
-exports.api = functions.https.onRequest(app);
