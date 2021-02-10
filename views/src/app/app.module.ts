bb5755ea034c2670b1f32f871ed21aadaeac001b
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2021-02-10 21:52:13 +1100

    Remove unused imports

--- views/src/app/app.module.ts
+++ views/src/app/app.module.ts
@@ -2,24 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CheckInComponent } from './check-in/check-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CheckInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule,
-    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
